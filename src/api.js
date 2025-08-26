0b49e2d2fdbe9f85352189d29c8149675e190f80
MightyP07 <promisebamgbola@gmail.com>
2025-08-26 14:55:13 +0100

    Delete src directory

--- src/api.js
+++
@@ -1,9 +0,0 @@
-const API_BASE =
-  import.meta.env.VITE_API_BASE_URL ||
-  (import.meta.env.MODE === "development"
-    ? "http://localhost:5000"
-    : "https://landlord-tenant-app.onrender.com");
-
-console.log("API_BASE is:", API_BASE);
-
-export default API_BASE;
