38a2529de0c822c141564d2f0f74299e9b95b2a3
MightyP07 <promisebamgbola@gmail.com>
2025-08-26 16:20:37 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,12 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: "./",
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      workbox: {
-        navigateFallback: '/index.html'
-      }
-    })
-  ]
+      workbox: { navigateFallback: '/index.html' },
+    }),
+  ],
 })
