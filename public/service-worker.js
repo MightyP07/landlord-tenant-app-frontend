4aa5efd7ff9e74bce7df69f4bc041bf7fb31ef2c
MightyP07 <promisebamgbola@gmail.com>
2025-08-27 06:42:39 +0100

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,20 +5,27 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
-  self.skipWaiting(); // activate new SW immediately
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     (async () => {
       const keys = await caches.keys();
-      await Promise.all(keys.map((key) => key !== CACHE_NAME && caches.delete(key)));
+      await Promise.all(
+        keys.map((key) => key !== CACHE_NAME && caches.delete(key))
+      );
 
-      // Tell clients a new version is available
-      const clientsArr = await self.clients.matchAll({ type: "window", includeUncontrolled: true });
-      clientsArr.forEach((client) => {
-        client.postMessage({ type: "NEW_VERSION" });
-      });
+      // Notify only once: send message only if this SW is brand new
+      if (self.registration.navigationPreload) {
+        const clientsArr = await self.clients.matchAll({
+          type: "window",
+          includeUncontrolled: true,
+        });
+        clientsArr.forEach((client) => {
+          client.postMessage({ type: "NEW_VERSION" });
+        });
+      }
     })()
   );
   self.clients.claim();
